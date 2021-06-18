59f445a4300cca08dd32caf3644ab0d9907f3f52
Dayakar Ravuri <dayakar16@gmail.com>
2021-06-17 20:49:55 -0400

    Adding the practice of Django

---
+++ pages/views.py
@@ -0,0 +1,16 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+# Create your views here.
+def Home_view(request,*args,**kwargs):
+    return render(request,"home.html",{})
+
+def contact_view(request,*args,**kwargs):
+    return render(request,"contact.html",{})
+
+def about_view(request,*args,**kwargs):
+    my_context = { "my_text" : "This is inside the about View",
+                   "my_number": 123,
+                   "my_list": [123,567,9035, "abc"],
+    }
+    return render(request,"about.html",my_context)
